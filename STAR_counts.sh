8e740c9466801ac918fee9b6605157d2a6108163
Zhou-geng Xu <xuzhougeng@163.com>
2020-10-12 11:05:53 +0800

    file originization

---
+++ STAR_counts.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+# Usage
+
+# Load STAR 
+#STAR=/opt/biosoft/STAR-2.6.1c/bin/Linux_x86_64/STAR
+module load STAR/2.6.1.d
+STAR=`which STAR`
+
+reference=$1
+sample=$2
+GTF=$3
+
+# building index for alignment
+index=STAR
+mkdir -p ${index}
+
+if [ ! -f ${index}/SA ]
+then
+    ${STAR} \
+    --runThreadN 50 \
+    --runMode genomeGenerate \
+    --genomeDir $index \
+    --genomeFastaFiles ${reference}
+fi
+# create outdir
+mkdir -p 01-clean-data
+mkdir -p 02-read-align
+
+# align RNA-seq to reference
+exec 0< $2
+while read prefix
+do
+	fq1=00-raw-data/${prefix}_1.fq.gz
+	fq2=00-raw-data/${prefix}_2.fq.gz
+	fastp -w 8 -i $fq1 -I $fq2 -o 01-clean-data/${prefix}_1.fq.gz -O 01-clean-data/${prefix}_2.fq.gz \
+		-j 01-clean-data/${prefix}.json -h 01-clean-data/${prefix}.html
+    STAR \
+    	--genomeDir $index \
+    	--runThreadN 100 \
+    	--readFilesIn 01-clean-data/${prefix}_1.fq.gz 01-clean-data/${prefix}_2.fq.gz \
+    	--readFilesCommand zcat \
+    	--outFileNamePrefix 02-read-align/${prefix}_ \
+    	--outSAMtype BAM SortedByCoordinate \
+    	--outBAMsortingThreadN 40 \
+        --quantMode GeneCounts --sjdbGTFfile $GTF
+
+done
+#if [ ! -f merged.bam ] ;then
+#	samtools merge merged.bam *sortedByCoord.out.bam
+#fi
---
+++ annotation/maker_rename.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+"""
+一个非常简单的脚本，主要目的是将maker_map_ids输出的编号，改成类似于AT1G10010这种形式
+输入参数为两个, maker_map_ids的输出文件, 还有你的物种名的缩写, 例如Athaliana-> AT, Alyrata->AL
+scaffold，则是用z表示
+"""
+
+import re
+import sys
+
+if len(sys.argv) < 3:
+    print("Usage: {} map.id prefix".format(sys.argv[0]))
+    sys.exit()
+
+gff = sys.argv[1]
+prefix = sys.argv[2]
+
+chr_name = ""
+gene_count = 1
+count = 0
+
+with open(gff, "r") as file:
+    lines = file.readlines()
+
+for line in lines:
+    raw_name = line.split("\t")[0]
+    if raw_name.find("scaffold") > 0:
+        break
+    count += 1
+
+    # if it has mRNA, keep the count unchange, and mRNA_count add 1
+    if raw_name.find("mRNA") >=0:
+        mRNA_count = raw_name.split("-")[6]
+        print("{}\t{}{}G".format(raw_name, prefix, chr_id) + str(gene_count).zfill(4) + "0." + str(mRNA_count ) )
+    else:
+        # check whether chrosome name is same as before
+        if chr_name == raw_name.split("-")[1]:
+            gene_count += 1
+            print("{}\t{}{}G".format(raw_name, prefix, chr_id) + str(gene_count).zfill(4) + "0" )
+        else:
+            chr_name = raw_name.split("-")[1]
+            chr_id = chr_name.lstrip("chr")
+            gene_count = 1
+            print("{}\t{}{}G".format(raw_name, prefix, chr_id) + str(gene_count).zfill(4) + "0" )
+
+gene_count = 0
+for line in lines[count:]:
+    raw_name = line.split("\t")[0]
+    chr_id = "z"
+    if raw_name.find("mRNA") >=0:
+        mRNA_count = raw_name.split("-")[6]
+        print("{}\t{}{}G".format(raw_name, prefix, chr_id) + str(gene_count).zfill(4) + "0." + str(mRNA_count ) )
+    else:
+        gene_count += 1
+        print("{}\t{}{}G".format(raw_name, prefix, chr_id) + str(gene_count).zfill(4) + "0" )
