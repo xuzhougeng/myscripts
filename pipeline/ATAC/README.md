422a2001e0a37e70ca471985701c41431741e00f
xuzhougeng <xuzhougeng@yeah.net>
2021-07-02 12:25:42 +0800

    ATAC update

--- pipeline/ATAC/README.md
+++ pipeline/ATAC/README.md
@@ -1,5 +1,4 @@
-
-使用方法
+## 使用方法
 
 第一步: 将项目克隆到本地
 
@@ -25,14 +24,36 @@ module load MACS/2.1.2
 第三步: 编辑config.yaml
 
 ```yaml
-FASTA: 参考基因组fasta的位置
+FASTA: 参考基因组fasta的位置, 
 GFF: 参考基因组gff所对应的位置，为空时，不绘制gene body相关图
 GENOMESIZE: 基因组大小, 例如 1.1e8 表示110M, 不能为空
 ADAPTER: "CTGTCTCTTATACACATCT"  接头序列
 SAMPLES: samples.txt 记录样本信息
+MAP_QUALITY: 10 最低比对质量
+REMOVE_PLASTOME: True/False/或者不包含质体的序列编号的txt文件
 ```
 
-第四步: 创建 samples.tsv, 文件格式如下
+FASTA对应的fasta文件名中间不要有多余的 "."，例如 A.thaliana.fa, 请改成 Athaliana.fa或 A_thaliana.fa
+
+REMOVE_PLASTOME: 
+
+如果输入的是False或者空白, 表示不过滤
+
+如果输入的是数字，则表示认为低于 X bp都是潜在的细胞器， 推荐200000
+
+一些质体的测序文章
+
+- https://www.frontiersin.org/articles/10.3389/fpls.2018.00493/full
+- https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7076112
+
+如果输入的是 bed 文件，那么就会只保留在 bed 文件里面的序列，每行一个，例如
+
+```txt
+chr1    0   1000000
+chr2    0   2000000
+```
+
+其中 samples.tsv, 文件格式如下
 
 ```text
 id,sample,rep,fq1,fq2
@@ -44,11 +65,17 @@ r2_2,s4,2,/path/to/s4.fastq.gz,/path/to/s4.fastq.gz
 
 id表示样本的编号，必须**唯一**, sample和rep分别表示样本编号和重复信息, fq1和fq2表示的fastq的位置
 
-第三步: 运行snakemake
+第四步: 运行snakemake
 
 ```bash
-#测试
+# 使用默认的config.yaml 测试
 snakemake -np
-# 运行
+# 使用默认的config.yaml
 snakemake -j 120
+# 使用给定的config文件
+snakemake -j 120 --configfile config_diy.yaml -s Snakefile
 ```
+
+
+
+
---
+++ pipeline/ATAC/bam_frag_stats.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "htslib/sam.h"
+#include <stdbool.h>
+#include <string.h>
+#include "htslib/khash.h"
+
+#define ARRSIZE 1001
+long frag_size[ARRSIZE] = {0};
+
+//hash set for qname de-duplication
+KHASH_SET_INIT_STR(str)
+
+bool is_bam(char *fname);
+int strcasecmp(const char *s1, const char *s2);
+char* get_fn_out(const char *fn);
+
+int main(int argc, char *argv[]){
+  //hts_verbose = 0; // suppressed htslib warnnings	
+
+  if ( argc == 1){
+    fprintf(stderr, "Usage: %s input.bam  [frag_size.txt] \n", argv[0]);
+    exit(0);
+  }
+  char *frag_size_fname;
+  if (argc == 2){
+    frag_size_fname = get_fn_out(argv[1]);
+  } else{
+    frag_size_fname = argv[2];
+  }
+  
+ 
+
+  // init hash set
+  khash_t(str) *qname_set;
+  qname_set = kh_init(str);
+  khint_t k;
+  int absent;
+
+
+  //load BAM file
+  char *bam_fn = argv[1];
+
+  if (! is_bam(bam_fn) ){
+        fprintf(stderr, "%s not a BAM file!\n", argv[1]);
+        exit(0);
+  } 
+  fprintf(stderr, "Process %s !\n", argv[1]);
+
+  samFile *fp_in = hts_open( bam_fn, "r" ); // open bam file
+  bam_hdr_t *bamHdr = sam_hdr_read(fp_in); // read header
+  bam1_t *aln = bam_init1(); // initialize an alignment
+  
+
+  //iterate the bam file
+  while ( sam_read1( fp_in, bamHdr, aln) > 0){
+    // filter the alignment which not on the same tig
+    if (aln->core.tid != aln->core.mtid) continue;
+    char *qname = bam_get_qname(aln);
+    
+    // check qname whether in the set or not
+    // if qname in the set, remove the qname, and the target-frag_size array +1
+    k = kh_get(str, qname_set, qname);
+    if (k == kh_end(qname_set)){
+      k = kh_put(str, qname_set, qname, &absent);
+      continue;
+    } 
+    kh_del(str,qname_set, k);
+
+    long insize = abs( aln->core.isize );
+    if (insize > ARRSIZE - 1){
+      ++frag_size[ARRSIZE-1];
+    } else{
+      ++frag_size[insize];
+    }
+  }
+
+  bam_destroy1(aln);
+  bam_hdr_destroy(bamHdr);
+  sam_close(fp_in);
+
+  // write the array
+  FILE *fp_out;
+  fp_out = fopen(frag_size_fname, "w");
+  long i ;
+  for (i = 0; i < ARRSIZE; ++i ){
+    fprintf(fp_out, "%ld\t%ld\n", i, frag_size[i] );
+  }
+  fclose(fp_out);
+
+
+}
+
+//replace the .bam to .txt
+char* get_fn_out(const char *fn){
+  int l = strlen(fn);
+  char *temp = malloc( (size_t)  l * sizeof(char) );
+  strcpy(temp, fn);
+  strcpy(temp+l-4, ".txt");
+  return temp;
+}
+
+bool is_bam(char *fname){
+  int l = strlen(fname);
+  if (l >= 4 && strcasecmp(fname+l-4, ".bam")!=0){
+    return false;
+  }
+  return true;
+}
+
+//comparsion string ignoreing case
+int strcasecmp(const char *s1, const char *s2){
+  int ret ;
+  char temp[10];
+  strcpy(temp, s1);
+  
+  int i;
+  for(i = 0 ; temp[i]!= '\0'; ++i){
+    temp[i] = tolower(temp[i]);
+  }
+
+  ret = strcmp(temp, s2);
+  return ret;
+}
