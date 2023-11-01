39d85b68a25c5e3e06109b4f72bdbbce27cc98ed
xuzhougeng <xuzhougeng@yeah.net>
2023-11-01 16:41:39 +0800

    update trinity process pipeline v0.01

--- annotation/gff2gtf.py
+++ annotation/gff2gtf.py
@@ -20,45 +20,158 @@ TO DO:add additional two features
 I only test this script in Arabidopsis_thaliana.TAIR10.44.gff3.gz
 """
 
-from sys import argv
 import re
+import sys
 
-for line in open(argv[1]):
-    line = line.strip()
-    if line.startswith("###"):
-        continue
-    if line.startswith("#"):
-        print(line)
-        continue
-    fields = line.split("\t")
-    feature = fields[2]
-    if feature == "gene":
-        gene_id = re.findall(r'gene_id=(.*?);',fields[8])[0]
-        fields[8] = "gene_id \"{}\";gene_name \"{}\";".format(gene_id, gene_id)
-    elif feature == "mRNA" or feature == "transcript":
-        tx_id = re.findall(r'transcript_id=(.*?)$',fields[8])[0]
-        gene_id = tx_id.split(".")[0]
-        fields[2] = "transcript"
-        fields[8] = "gene_id \"{}\";transcript_id \"{}\";gene_name \"{}\";".format(gene_id, tx_id, gene_id)
-    elif feature == "CDS":
-        tx_id = re.findall(r'protein_id=(.*?)$',fields[8])[0]
-        gene_id = tx_id.split(".")[0]
-        fields[8] = "gene_id \"{}\";transcript_id \"{}\";gene_name \"{}\";".format(gene_id, tx_id, gene_id)
-    elif feature == "exon":
-        exon_id = re.findall(r'exon_id=(.*?);',fields[8])[0]
-        tx_id = ".".join(exon_id.split(".")[0:2])
-        gene_id = tx_id.split(".")[0]
-        fields[8] = "gene_id \"{}\";transcript_id \"{}\";gene_name \"{}\";".format(gene_id, tx_id, gene_id)
-    elif feature == "five_prime_UTR" or feature == "five_prime_utr" or feature == "5UTR":
-        fields[2] = "five_prime_utr"
-        tx_id = fields[8].split(":")[1]
-        gene_id = tx_id.split(".")[0]
-        fields[8] = "gene_id \"{}\";transcript_id \"{}\";gene_name \"{}\";".format(gene_id, tx_id, gene_id)
-    elif feature == "three_prime_UTR" or feature == "three_prime_utr" or feature == "3UTR":
-        fields[2] = "three_prime_utr"
-        tx_id = fields[8].split(":")[1]
-        gene_id = tx_id.split(".")[0]
-        fields[8] = "gene_id \"{}\";transcript_id \"{}\";gene_name \"{}\";".format(gene_id, tx_id, gene_id)
+def ensemble_style(input_file):
+
+    for line in open(input_file):
+        line = line.strip()
+        if line.startswith("###"):
+            continue
+        if line.startswith("#"):
+            print(line)
+            continue
+        fields = line.split("\t")
+        feature = fields[2]
+        if feature == "gene":
+            gene_id = re.findall(r'gene_id=(.*?);',fields[8])[0]
+            fields[8] = "gene_id \"{}\";gene_name \"{}\";".format(gene_id, gene_id)
+        elif feature == "mRNA" or feature == "transcript":
+            tx_id = re.findall(r'transcript_id=(.*?)$',fields[8])[0]
+            gene_id = tx_id.split(".")[0]
+            fields[2] = "transcript"
+            fields[8] = "gene_id \"{}\";transcript_id \"{}\";gene_name \"{}\";".format(gene_id, tx_id, gene_id)
+        elif feature == "CDS":
+            tx_id = re.findall(r'protein_id=(.*?)$',fields[8])[0]
+            gene_id = tx_id.split(".")[0]
+            fields[8] = "gene_id \"{}\";transcript_id \"{}\";gene_name \"{}\";".format(gene_id, tx_id, gene_id)
+        elif feature == "exon":
+            exon_id = re.findall(r'exon_id=(.*?);',fields[8])[0]
+            tx_id = ".".join(exon_id.split(".")[0:2])
+            gene_id = tx_id.split(".")[0]
+            fields[8] = "gene_id \"{}\";transcript_id \"{}\";gene_name \"{}\";".format(gene_id, tx_id, gene_id)
+        elif feature == "five_prime_UTR" or feature == "five_prime_utr" or feature == "5UTR":
+            fields[2] = "five_prime_utr"
+            tx_id = fields[8].split(":")[1]
+            gene_id = tx_id.split(".")[0]
+            fields[8] = "gene_id \"{}\";transcript_id \"{}\";gene_name \"{}\";".format(gene_id, tx_id, gene_id)
+        elif feature == "three_prime_UTR" or feature == "three_prime_utr" or feature == "3UTR":
+            fields[2] = "three_prime_utr"
+            tx_id = fields[8].split(":")[1]
+            gene_id = tx_id.split(".")[0]
+            fields[8] = "gene_id \"{}\";transcript_id \"{}\";gene_name \"{}\";".format(gene_id, tx_id, gene_id)
+        else:
+            continue
+        print("\t".join(fields))
+
+def re_find(name, string):
+    pattern = r'{}=(.*?)(;|$)'.format(name)
+    matches = re.findall(pattern, string)
+    if matches:
+        return matches
     else:
-        continue
-    print("\t".join(fields))
+        return None
+
+def star_style(input_file, output_file=None):
+
+    gene_attr_dict = {} # gene : gene_name
+    gene_dict = {} # gene [ tx1, tx2, ...]
+    tx_dict = {}   # tx : [exon1, exon2, ...]
+    exon_dict = {} # exon: [chr, start, end, strand]
+
+    for line in open(input_file):
+        line = line.strip()
+        if line.startswith("##"):
+            continue
+        
+        fields = line.split("\t")
+        feature = fields[2]
+        if feature == "gene":
+            # exctrat gene_id from ID
+            matches  = re_find("ID", fields[8])
+            if matches is None:
+                raise ValueError("No ID found in gene feature.")
+            gene_id = matches[0][0]
+
+            # extract gene_name from Name
+            gene_name = re_find("Name", fields[8])[0][0]
+            gene_attr_dict[gene_id] = gene_name
+
+        elif feature == "mRNA" or feature == "transcript":
+            # extract transcript_id from ID
+            matches = re_find("ID", fields[8])
+            if matches is None:
+                raise ValueError("No ID found in mRNA feature.")
+            tx_id = matches[0][0]
+            # extract gene_id from Parent
+            matches = re_find("Parent", fields[8])
+            if matches is None:
+                raise ValueError("No Parent found in mRNA feature.")
+            gene_id = matches[0][0]
+            
+            if gene_id not in gene_dict:
+                gene_dict[gene_id] = [tx_id]
+            else:
+                gene_dict[gene_id].append(tx_id)
+        elif feature == "exon":
+            # extract transcript_id from ID
+            matches = re_find("ID", fields[8])
+            if matches is None:
+                raise ValueError("No ID found in exon feature.")
+            exon_id = matches[0][0]
+            # extract gene_id from Parent
+            matches = re_find("Parent", fields[8])
+            if matches is None:
+                raise ValueError("No Parent found in exon feature.")
+            tx_id = matches[0][0]
+
+            if tx_id not in tx_dict:
+                tx_dict[tx_id] = [exon_id]
+            else:
+                tx_dict[tx_id].append(exon_id)
+            exon_dict[exon_id] = [fields[0], fields[3], fields[4], fields[6]]
+
+    if output_file is None:
+        output = sys.stdout
+    else:
+        output = open(output_file, "w")
+
+    for gene,tx_ids in gene_dict.items():
+        gene_name = gene_attr_dict[gene]
+
+        for tx_id in tx_ids:
+            exon_ids = tx_dict[tx_id]
+            for exon_id in exon_ids:
+                chrom, start, end, strand = exon_dict[exon_id]
+                output.write("{}\tSTAR\texon\t{}\t{}\t.\t{}\t.\tgene_id \"{}\"; transcript_id \"{}\"; gene_name \"{}\";\n".format(chrom, start, end, strand, gene, tx_id, gene_name))
+
+
+    if output_file is not None:
+        output.close()
+
+
+def main(args):
+    input_file = args.input
+    output_file = args.output
+
+    if args.format == "ensemble":
+        ensemble_style(input_file)
+    elif args.format.lower() == "star":
+        star_style(input_file, output_file)
+    else:
+        print("Please provide the correct format.")
+        exit(1)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="This script convert the gff to gtf.")
+
+    parser.add_argument("-i", "--input", type=str, required=True, help="input gff file")
+    parser.add_argument("-o", "--output", type=str, required=False, help="output gtf file")
+    parser.add_argument("-f", "--format", type=str, required=True, choices=["ensemble", "star"], help="out gff format")
+    args = parser.parse_args()
+
+    main(args)
+
+    
--- pipeline/transcriptome-assembly-pipeline/trinity_post_process.py
+++ pipeline/transcriptome-assembly-pipeline/trinity_post_process.py
@@ -82,9 +82,6 @@ def stat(g_id_dict, t_id_dict):
     print("平均预测：", average_transcript_count)
     print("最多预测：", max_transcript_count)
 
-
-
-
 def filter_proteins(g_id_dict, t_id_dict, p_id_dict):
     """
     Filters out incomplete proteins from a dictionary of gene IDs, transcript IDs, and protein IDs.
@@ -106,16 +103,14 @@ def filter_proteins(g_id_dict, t_id_dict, p_id_dict):
     g_id_to_delete = []
     t_id_to_delete = []
 
-    for g_id in g_id_dict:
+    for g_id in g_id_dict.keys():
 
         t_ids = g_id_dict[g_id]
         complete_tx = 0 # flag for whether the gene is complete
-        print(t_ids)
 
         for t_id in t_ids:
 
             p_ids = t_id_dict[t_id]
-            print(p_ids)
 
             for p_id in p_ids:
                 if len(p_id) == 0:
@@ -168,7 +163,7 @@ def filter_proteins(g_id_dict, t_id_dict, p_id_dict):
     
     return g_id_dict, t_id_dict
 
-def extract_fasta_to_file(output_file, fasta_file, g_id_dict, t_id_dict):
+def extract_fasta_to_file(output_file, fasta_file, g_id_dict, t_id_dict, prefix=None):
     """
     Extracts transcripts from a CDS file and writes them to an output file.
 
@@ -181,6 +176,12 @@ def extract_fasta_to_file(output_file, fasta_file, g_id_dict, t_id_dict):
     Returns:
     None
     """
+    
+    gene_count = 0
+    new_name_dict = {}
+
+    delim = "."
+
     with open(output_file, "w") as f:
         for record in SeqIO.parse(fasta_file, "fasta"):
             p_id = record.id
@@ -197,11 +198,20 @@ def extract_fasta_to_file(output_file, fasta_file, g_id_dict, t_id_dict):
 
             if p_id in t_id_dict[t_id]:
                 # update the header with transcript ID
-                record.id = t_id
+                if prefix is  None:
+                    record.id = t_id
+                else:
+                    if g_id not in new_name_dict:
+                        gene_count += 1
+                        new_name_dict[g_id] = f"{prefix}{delim}G{gene_count:05d}"
+                    record.id = new_name_dict[g_id] + {delim} + t_id.split("_")[-1]
+
                 record.description = ""
 
                 # write the record to output file
                 SeqIO.write(record, f, "fasta")
+
+    return new_name_dict
 
 
 def save_gene_transcript_mapping_to_file(out_file, g_id_dict):
@@ -223,28 +233,145 @@ def save_gene_transcript_mapping_to_file(out_file, g_id_dict):
                 f.write(g_id + "\t" + t_id + "\n")
 
 
-def main(cds_in, pep_in, cds_out, pepe_out, out_mapping_file):
+def transdecoder_filter(args):
+    cds_in = args.cds_in
+    cds_out = args.cds_out
+    pep_in = args.pep_in
+    pepe_out = args.pep_out
+    prefix = args.prefix
+    mapping_file = args.mapping
 
-    p_id_dict, t_id_dict, g_id_dict = parse_cds_file(cds_in)
+    g_id_dict, t_id_dict, p_id_dict = parse_cds_file(cds_in)
 
     stat(g_id_dict, t_id_dict)
 
     g_id_dict, t_id_dict = filter_proteins(g_id_dict, t_id_dict, p_id_dict)
 
-    extract_fasta_to_file(cds_out, cds_in, g_id_dict, t_id_dict)
-    extract_fasta_to_file(pepe_out, pep_in, g_id_dict, t_id_dict)
+    new_name_dict = extract_fasta_to_file(cds_out, cds_in, g_id_dict, t_id_dict, prefix)
+    if len(new_name_dict) > 0:
+        with open(mapping_file, "w") as f:
+            for k,v in new_name_dict.items():
+                 f.write(f"{k}\t{v}\n")
 
-    save_gene_transcript_mapping_to_file(out_mapping_file, g_id_dict)
+    extract_fasta_to_file(pepe_out, pep_in, g_id_dict, t_id_dict, prefix)
+
+    #save_gene_transcript_mapping_to_file(out_mapping_file, g_id_dict, prefix)
+
+def trinity_extract(args):
+    fasta_file = args.input
+    output_file = args.output
+    mapping_file = args.mapping
+
+    # read mapping file
+    mapping_dict = {}
+    with open(mapping_file, "r") as f:
+        for line in f:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            items = line.split("\t")
+            mapping_dict[items[0]] = items[1]
+    
+    # read trinity.fasta file
+    with open(output_file, "w") as f:
+        for record in SeqIO.parse(fasta_file, "fasta"):
+            t_id = record.id
+            g_id = "_".join(t_id.split("_")[:-1])
+
+            # if gene id deleted , skip
+            if not g_id in mapping_dict:
+                continue
+
+            record.id = mapping_dict[g_id] + "_" + t_id.split("_")[-1]  
+
+            record.description = ""
+
+            # write the record to output file
+            SeqIO.write(record, f, "fasta")
+
+def longest_extract(args):
+    """ extract the longest cds/pep for each gene from input.fasta
+    """
+    fasta_file = args.input
+    output_file = args.output
+    
+    gene_dict = {}
+    with open(fasta_file, "r") as f:
+        for record in SeqIO.parse(f, "fasta"):
+            t_id = record.id
+            t_size = len(record.seq)
+            g_id = "_".join(t_id.split("_")[:-1])
+            if g_id in gene_dict:
+                if gene_dict[g_id][1] < t_size:
+                    gene_dict[g_id] = (t_id, t_size, record.seq)
+            else:
+                gene_dict[g_id] = (t_id, t_size, record.seq)
+    
+    # write the longest transcript to output file
+    with open(output_file, "w") as f:
+        for k,v in gene_dict.items():
+            f.write(f">{v[0]}\n{v[2]}\n")
+
+
+def refine_lace_gff(args):
+    """
+    refine lace gff file for STAR
+    """
+    gff_file = args.input
+
+    with open(gff_file, "r") as f:
+        for line in f:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            items = line.split("\t")
+            if items[2] == "gene":
+                items[2] = "transcript"
+                items[8] = items[8].replace("gene_id", "transcript_id")
+                print("\t".join(items))
+            elif items[2] == "transcript":
+                items[2] = "exon"
+                items[8] = items[8].replace("transcript_id", "exon_id")
+                print("\t".join(items))
+            else:
+                print(line) 
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="This script filters out incomplete proteins from a Trinity CDS file.")
-    parser.add_argument("-c", "--cds_in", type=str, help="The path to the Trinity CDS file.", required=True)
-    parser.add_argument("-p", "--pep_in", type=str, help="The path to the Trinity protein file.", required=True)
-    parser.add_argument("-o", "--cds_out", type=str, help="The path to the output CDS file.", required=True)
-    parser.add_argument("-e", "--pep_out", type=str, help="The path to the output protein file.", required=True)
-    parser.add_argument("-m", "--out_mapping_file", type=str, help="The path to the output mapping file.", required=True)
-    args = parser.parse_args()
 
-    main(args.cds_in, args.pep_in, args.cds_out, args.pep_out, args.out_mapping_file)
+    subparsers = parser.add_subparsers(title='subcommands', description='valid subcommands', help='additional help')
+
+    # subparser for the filter command
+    parser_filter = subparsers.add_parser('filter', help='filter transdecoder cds and pepe file ') 
+    parser_filter.add_argument("-c", "--cds_in", type=str, help="The path to the Trinity CDS file.", required=True)
+    parser_filter.add_argument("-p", "--pep_in", type=str, help="The path to the Trinity protein file.", required=True)
+    parser_filter.add_argument("-o", "--cds_out", type=str, help="The path to the output CDS file.", required=True)
+    parser_filter.add_argument("-e", "--pep_out", type=str, help="The path to the output protein file.", required=True)
+    parser_filter.add_argument("--prefix", type=str, help="prefix for cds/pep ", required=False)
+    parser_filter.add_argument("-m", "--mapping", type=str, help="The path to the output mapping file.", required=False)
+    parser_filter.set_defaults(func=transdecoder_filter)  
+
+    # subparser for the extract command
+    parser_extract = subparsers.add_parser('extract', help='extract Trinity.fasta from map.txt generated by filter')
+    parser_extract.add_argument("-i", "--input", type=str, help="The path to the Trinity.fasta file.", required=True)
+    parser_extract.add_argument("-o", "--output", type=str, help="The path to the output file.", required=True)
+    parser_extract.add_argument("-m", "--mapping", type=str, help="The path to the mapping file.", required=True)
+    parser_extract.set_defaults(func=trinity_extract)
+
+    # subparser for the longest command
+    parser_longest = subparsers.add_parser('longest', help='extract longest transcript from Trinity.fasta')
+    parser_longest.add_argument("-i", "--input", type=str, help="The path to the input.fasta file.", required=True)
+    parser_longest.add_argument("-o", "--output", type=str, help="The path to the output file.", required=True)
+    parser_longest.set_defaults(func=longest_extract)
+    # subparser for 
+
+
+
+    args = parser.parse_args()
+    
+    if hasattr(args, 'func'):
+        args.func(args)
+    else:
+        parser.print_help()
