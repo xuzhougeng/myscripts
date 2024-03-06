a2960cd7c7787ce64ab9e99b5c854c05dc001b72
xuzhougeng <xuzhougeng@yeah.net>
2024-03-06 16:42:49 +0800

    fix typora

--- misc/gff_utils.py
+++ misc/gff_utils.py
@@ -90,11 +90,11 @@ def build_gff3_lines(gff_dict):
     return lines
 
 
-def reformat_gff(gff_filename, output_fielname):
+def reformat_gff(gff_filename, output_filename):
     gff_dict = read_gff3(gff_filename)
     add_ids_to_features(gff_dict['other'])
     gff_lines = build_gff3_lines(gff_dict)
-    with open(output_fielname, 'w') as f:
+    with open(output_filename, 'w') as f:
         for line in gff_lines:
             f.write(line + '\n')
 
